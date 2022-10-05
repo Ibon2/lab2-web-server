658a37c7941ebbd8934c11bab3256d8ee8bef3a0
Ibon2 <ibonlkz@gmail.com>
2022-10-05 14:46:23 +0200

    otro

--- src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
+++ src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
@@ -1,10 +1,9 @@
 package es.unizar.webeng.lab2
 
-import java.time.LocalDateTime
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-
+import java.time.LocalDateTime
 
 class TimeDTO(val time: LocalDateTime)
 
